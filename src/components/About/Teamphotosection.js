6747f0280fb32fae2fbbf69970ad30ff5902ff44
Rithesh-Rithu <ritheshdavid@gmail.com>
2020-04-17 12:49:04 +0530

    initial commit

---
+++ src/components/About/Teamphotosection.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export default function Teamphotosection() {
+    return (
+        <div className="row">
+            <div className="col-10 col-sm-10 mx-auto">
+            <div className="card-group">
+                <div className="card">
+                    <img src="https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?cs=srgb&dl=woman-wearing-yellow-adidas-shirt-1759622.jpg&fm=jpg" className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Name 1</h5>
+                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src="https://images.pexels.com/photos/285171/pexels-photo-285171.jpeg?cs=srgb&dl=beautiful-eyewear-fashion-female-285171.jpg&fm=jpg" className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Name 2</h5>
+                        <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src="https://images.pexels.com/photos/974911/pexels-photo-974911.jpeg?cs=srgb&dl=woman-holding-two-white-and-beige-shopping-paper-bags-in-974911.jpg&fm=jpg" className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Name 3</h5>
+                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                        <p className="card-text"><small classNameName="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,56 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Layout from "../components/layout"
+import HeroSection from "../components/Reuseable/HeroSection"
+import Infoblock from "../components/Reuseable/Infoblock"
+import DualInfoblock from "../components/Reuseable/DualInfoblock"
+import Coursecart from '../components/Cart/Coursecart'
+import { graphql, StaticQuery } from 'gatsby'
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <HeroSection 
+      img={data.img.childImageSharp.fluid}
+      title="i write code" 
+      subtitle="learncodeonline.in" 
+      heroclass="hero-background" 
+    />
+    <Infoblock heading="About Us" /> 
+    <Coursecart courses={data.courses}/>
+    <DualInfoblock heading="Our Team" img="https://images.pexels.com/photos/247204/pexels-photo-247204.jpeg?cs=srgb&dl=portrait-of-a-woman-247204.jpg&fm=jpg" />
   </Layout>
+
 )
+
+export const query = graphql`
+{
+  img: file(relativePath: { eq: "heromain.jpg" }) {
+     childImageSharp {
+       fluid(quality: 100) {
+        ...GatsbyImageSharpFluid_tracedSVG
+       }
+     }
+   }
+   courses: allContentfulCourses {
+    edges{
+      node{
+        id
+        title
+        price
+        category
+        description{
+          description
+        }
+        image{
+          fixed(width:200,height:120){
+            ...GatsbyContentfulFixed_tracedSVG
+          }
+        }
+      }
+    }
+}
+}
+`
 
 export default IndexPage
